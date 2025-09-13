5c4ebae1ee33a8733968e8b1f5f616f238d747c1
Nikhil Kumar <nikhil76503@gmail.com>
2025-09-13 19:56:53 +0530

    configured  trpc in next

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,17 @@
+import {fetchRequestHandler} from '@trpc/server/adapters/fetch'
+import { authOptions } from "@/lib/auth";
+import { appRouter } from "@/server";
+import { getServerSession } from "next-auth";
+import {type NextRequest } from "next/server";
+
+const handler = async (req: NextRequest) =>{
+    const session = await getServerSession(authOptions)
+
+    return fetchRequestHandler({
+        endpoint: '/api/trpc',
+        req,
+        router: appRouter,
+        createContext: ()=>({req,session}),
+    })
+}
+export {handler as GetAnimationsOptions, handler as POST}
